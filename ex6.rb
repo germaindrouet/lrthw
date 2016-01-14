2a5eb238b1718cbbd57325367dec3d8cb8b0ca33
Drouet <germain.drouet@gmail.com>
2016-01-14 17:34:31 +0100

    Fini les exercices 1 à 3

---
+++ ex6.rb
@@ -0,0 +1,34 @@
+# cataloguage
+types_of_people = 10
+# intro
+x = "There are #{types_of_people} types of people."
+# les logiques
+binary = "binary"
+# et
+do_not = "don't"
+# les innocents
+y = "Those who know #{binary} and those who #{do_not}."
+# les uns
+puts x
+# les autres
+puts y
+
+# reprenons
+puts "I said: #{x}."
+# et aussi
+puts "I also said: '#{y}'."
+
+# true
+hilarious = false
+# kinda yes
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# ikr
+puts joke_evaluation
+
+# Conclusion
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
---
+++ ex7.rb
@@ -0,0 +1,27 @@
+# les ligne 2 et 5 disent de faire apparaitre le texte suivant
+puts "Mary had a little lamb."
+# la ligne 4 dise de faire apparaitre le texte suivant et de faire a paraitre le texte dans la fonction snow probablement pour lui ajouter un attribue couleur texte blanc par la suite je suppose
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# faire apparaitre 10 points de suspension
+puts "." * 10  # what'd that do? look up!
+
+# fait prendre des valeurs aux mots clés endx
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+# il n'y as aucune différence visuel entre la commande print et puts
+
